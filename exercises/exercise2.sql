bce5d2c4c3333c1b34f91d020f2fb147efe72120
Jirka <jirka@Jirkas-MacBook-Pro.local>
2019-08-14 12:08:28 +0200

    rozsireni sql insertu

--- exercises/exercise2.sql
+++ exercises/exercise2.sql
@@ -81,7 +81,7 @@ create table publishers
 pub_id int PRIMARY KEY,
 pub_name varchar(50) NOT NULL,
 City varchar(50) NOT NULL,
-Phone_No NUMBER(15)
+Phone_No varchar(15)
 );
 
 --krok 3
@@ -161,3 +161,37 @@ INSERT INTO employees (surname,employee_name,birth_date,emp_date) VALUES('Brown'
 INSERT INTO employees (surname,employee_name,birth_date,emp_date) VALUES('Bays','Bonnie',to_date('1984/03/18','yyyy/mm/dd'),to_date('2002/03/01','yyyy/mm/dd'));
 INSERT INTO employees (surname,employee_name,birth_date,emp_date) VALUES('Small','Elisabeth',to_date('1983/06/07','yyyy/mm/dd'),to_date('2002/09/15','yyyy/mm/dd'));
 INSERT INTO employees (surname,employee_name,birth_date,emp_date) VALUES('Brand','Anne',to_date('1970/08/17','yyyy/mm/dd'),to_date('2001/03/04','yyyy/mm/dd'));
+
+INSERT INTO publishers (pub_name, City, Phone_No) VALUES ('Berkley Publishing','Boston','635-12-09');
+INSERT INTO publishers (pub_name, City, Phone_No) VALUES ('Course Technology', 'New York','025-22-03');
+INSERT INTO publishers (pub_name, City, Phone_No) VALUES ('Touchstone Books', 'Westport CT','635-42-11');
+INSERT INTO publishers (pub_name, City, Phone_No) VALUES ('Dom Ksi¹¿ki', 'Poznañ','775-24-92');
+INSERT INTO publishers (pub_name, City, Phone_No) VALUES ('Penguin USA', 'New York','305-32-34');
+
+INSERT INTO Books VALUES(1,1,'Electric Light',45.50,320,'science');
+INSERT INTO Books VALUES(2,2,'A Guide to SQL',68.90, 240,'science');
+INSERT INTO Books VALUES(3,3,'Travels with Charley',39.90, 120, 'novel');
+INSERT INTO Books VALUES(4,3,'Band of Brothers', 62.70, 359,'historical');
+INSERT INTO Books VALUES(5,3,'Shortest Poems', 55.20, 322,'poems');
+INSERT INTO Books VALUES(6,4,'Harry Potter and the Prisoner of Azkaban',29.00, 102, 'science fiction');
+INSERT INTO Books VALUES(7,4,'Harry Potter and the Goblet of Fire', 21.30, 89, 'science fiction');
+INSERT INTO Books VALUES(8,5,'Little Wind', 19.20, 55, 'for kids');
+INSERT INTO Books VALUES(9,5,'Nine Stories', 15.90, 35, 'for kids');
+INSERT INTO Books VALUES(10,2,'SQL for Dummies', 85.90, 210, 'science');
+INSERT INTO Books VALUES(11,3,'East of Eden', 23.20, 384, 'novel');
+INSERT INTO Books VALUES(12,3,'Van Gogh and Gauguin', 25.40, 245, 'historical');
+
+INSERT INTO Book_loans VALUES(1,'SJ123',4,to_date('2008/03/01','yyyy/mm/dd'),to_date('2008/07/28','yyyy/mm/dd'), 25.50);
+INSERT INTO Book_loans VALUES(2,'WJ090',2,to_date('2008/03/04','yyyy/mm/dd'),to_date('2008/06/18','yyyy/mm/dd'),5.20);
+INSERT INTO Book_loans (BookID,CardNo,emp_id,DateOut,DueDate) VALUES(3,'CA009',2,to_date('2008/03/04','yyyy/mm/dd'),to_date('2008/03/20','yyyy/mm/dd'));
+INSERT INTO Book_loans (BookID,CardNo,emp_id,DateOut,DueDate) VALUES(8,'CA009',1,to_date('2008/03/20','yyyy/mm/dd'),to_date('2008/04/10','yyyy/mm/dd'));
+INSERT INTO Book_loans (BookID,CardNo,emp_id,DateOut,DueDate) VALUES(8,'WJ090',1,to_date('2008/04/12','yyyy/mm/dd'),to_date('2008/04/30','yyyy/mm/dd'));
+INSERT INTO Book_loans (BookID,CardNo,emp_id,DateOut,DueDate) VALUES(4,'BA111',6,to_date('2008/04/15','yyyy/mm/dd'),to_date('2008/06/12','yyyy/mm/dd'));
+INSERT INTO Book_loans (BookID,CardNo,emp_id,DateOut,DueDate) VALUES(5,'BA111',6,to_date('2008/04/15','yyyy/mm/dd'),to_date('2008/06/12','yyyy/mm/dd'));
+INSERT INTO Book_loans (BookID,CardNo,emp_id,DateOut,DueDate) VALUES(5,'CC212',6,to_date('2008/06/21','yyyy/mm/dd'),to_date('2008/07/29','yyyy/mm/dd'));
+INSERT INTO Book_loans (BookID,CardNo,emp_id,DateOut,DueDate) VALUES(10,'CC212',6,to_date('2008/06/21','yyyy/mm/dd'),to_date('2008/08/08','yyyy/mm/dd'));
+INSERT INTO Book_loans VALUES(10,'SJ123',7,to_date('2008/08/21','yyyy/mm/dd'),to_date('2008/11/09','yyyy/mm/dd'),8.80);
+INSERT INTO Book_loans VALUES(4,'CA009',7,to_date('2008/08/22','yyyy/mm/dd'),to_date('2008/12/18','yyyy/mm/dd'),7.5);
+INSERT INTO Book_loans (BookID,CardNo,emp_id,DateOut,DueDate) VALUES(2,'CC212',8,to_date('2008/11/16','yyyy/mm/dd'),to_date('2009/01/19','yyyy/mm/dd'));
+INSERT INTO Book_loans (BookID,CardNo,emp_id,DateOut,DueDate) VALUES(2,'CC212',8,to_date('2008/11/17','yyyy/mm/dd'), NULL);
+INSERT INTO Book_loans (BookID,CardNo,emp_id,DateOut,DueDate) VALUES(11,'WJ090',9,to_date('2008/11/21','yyyy/mm/dd'), NULL);
